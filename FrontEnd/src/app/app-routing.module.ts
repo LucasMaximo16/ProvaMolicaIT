5a1378f9c860c4e4d57eb3629cb2913293bb96c4
LucasMaximo16 <lucasmaximodeandrade@gmail.com>
2022-11-17 22:29:23 -0300

    Projeto API cadastro de pessoas finalizado

---
+++ FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PessoaAlterarComponent } from './views/pessoa/pessoa-alterar/pessoa-alterar.component';
+import { PessoaListaComponent } from './views/pessoa/pessoa-lista/pessoa-lista.component';
+import { PessoaNovoComponent } from './views/pessoa/pessoa-novo/pessoa-novo.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PessoaListaComponent
+  },
+  {
+    path: 'pessoas',
+    component: PessoaListaComponent
+  },
+  {
+    path: 'pessoas/novo',
+    component: PessoaNovoComponent
+  },
+  {
+    path: 'pessoas/alterar/:id',
+    component: PessoaAlterarComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
